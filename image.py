0a0d9dcbf5b39af5e6134f2aa16710571a2277f0
pizzalord22 <31384712+pizzalord22@users.noreply.github.com>
2017-11-01 15:58:46 +0100

    Add files via upload

--- image.py
+++ image.py
@@ -1,13 +1,24 @@
 from PIL import ImageGrab
+from PIL import Image
 from time import *
+from datetime import datetime
+
+# x_pad is the x coordinate where the game screen starts
+# y_pad is the y coordinate where the game screen starts
+# all the coordinates are based on a 1009x by a 488y game screen
+# rgb color when dead 100, 102, 53
+x_pad = 82
+y_pad = 104
+dead_pixel = x_pad+31, y_pad+39
 
 
 def snapshot():
-    sleep(5)
-    for i in range(10):
-        img = ImageGrab.grab()
-        crop_rectangle = (65+17, 85+19, 1094, 595)
-        img = img.crop(crop_rectangle)
-        img.save("game"+str(i)+".jpg", "JPEG")
-        sleep(5)
+    sleep(1)
+    img = ImageGrab.grab()
+    crop_rectangle = (x_pad, y_pad, x_pad+1010, y_pad+489)
+    img = img.crop(crop_rectangle)
+    img.save("images/snapshot_"+str(datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))+".jpg", "JPEG")
+    print("snapshot taken")
+    return img
+
 
--- masin_bot.py
+++ masin_bot.py
@@ -6,51 +6,64 @@ import time
 import numpy
 import pywin32_system32
 from PIL import Image
+from PIL import ImageGrab
 import image as img
 
-img.snapshot()
+x_pad = 82
+y_pad = 104
+dead_pixel = x_pad+31, y_pad+39
 
 stat = "agi"
 
 
+def pres_key(key, sleep_time=0.1, amount=1, delay=0.01):
+    for i in range(amount):
+        keyDown(key)
+        time.sleep(float(sleep_time))
+        keyUp(key)
+        print(key, "was pressed", i+1, "time(s) for", sleep_time, "with a delay of", delay, "seconds between presses")
+
+
+def mouse_click(x_coord, y_coord, move_time=0.2, button="left", amount=1):
+    moveTo(x_pad+x_coord,y_pad+y_coord, move_time)
+    for i in range(amount):
+        click(button=button)
+    print("moved mouse to x:" + str(x_pad+x_coord), "y:" + str(y_pad+y_coord), "clicked the", button, "mouse button", amount, "times")
+
+
 def area1():
-    epoch_time_start = int(time.time())
-    epoch_time_end = epoch_time_start + 120
-    print("start time", epoch_time_start, "end time", epoch_time_end)
-    x, y = size()
-    x, y = x/2, y/1.7
+    # epoch_time_start = int(time.time())
+    # epoch_time_end = epoch_time_start + 120
+    # print("start time", epoch_time_start, "end time", epoch_time_end)
     time.sleep(3)
-    keyDown("1")
-    keyUp("1")
-    keyDown("1")
-    keyUp("1")
-    keyDown("up")
-    time.sleep(0.75)
-    keyUp("up")
-    moveTo(x, y, 0.1)
-    click(button="right")
-    time.sleep(7.5)
-    keyDown("right")
-    time.sleep(0.75)
-    keyUp("right")
-    keyDown("a")
-    moveTo(x+20, y - 100, 0.1)
-    click()
-    keyUp("a")
-    time.sleep(5)
-    keyDown("shift")
-    keyDown("a")
-    keyUp("a")
-    while int(time.time()) < epoch_time_end:
-        moveTo(x - 250, y - 100, 0.1)
-        click()
-        moveTo(x, y - 100, 0.1)
-        click()
-    keyUp("shift")
-    click()
-    time.sleep(1)
-    print("code run getting stats")
-    stats(stat)
+    pres_key("1", 0.1, 2)
+    pres_key("up", 0.5, 1)
+    img.snapshot()
+    # moveTo(x, y, 0.1)
+    # click(button="right")
+    # time.sleep(7.5)
+    # keyDown("right")
+    # time.sleep(0.75)
+    # keyUp("right")
+    # keyDown("a")
+    # moveTo(x+20, y - 100, 0.1)
+    # click()
+    # keyUp("a")
+    # time.sleep(5)
+    # keyDown("shift")
+    # keyDown("a")
+    # keyUp("a")
+    # image = img.snapshot()
+    #while image.getpixel(dead_pixel) != (100, 102, 53):
+    #    moveTo(x - 250, y - 100, 0.1)
+    #    click()
+    #    moveTo(x, y - 100, 0.1)
+    #    click()
+    #    img.snapshot()
+    # keyUp("shift")
+    # click()
+    # time.sleep(1)
+    # stats(stat)
 
 
 def stats(stat):
@@ -80,7 +93,13 @@ def stats(stat):
 def gui():
     root = Tk()
     root.title("Masin bot v1")
-    area1 = Button(root, text='area 1', width=25, command=area1)
-    area1.pack()
+    area1_button = Button(root, text='area 1', width=25, command=area1)
+    snapshot_button = Button(root, text="snapshot", width=25, command=img.snapshot)
+    area1_button.pack()
+    snapshot_button.pack()
     root.mainloop()
 
+
+#gui()
+
+mouse_click(0, 0, 0.2, "left", 1)
